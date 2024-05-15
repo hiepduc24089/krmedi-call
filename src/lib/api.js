f23068f2f086804e0570af6b170df8588e36e8c2
Egamorft <huytung.htbn@gmail.com>
2024-05-15 14:49:11 +0700

    change api url & laravel echo config

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://krmedi.vn:81';
+export const API_URL = 'https://krmedi.vn';
 
 const api = axios.create({
   baseURL: API_URL,
--- src/lib/pusher.js
+++ src/lib/pusher.js
@@ -6,10 +6,6 @@ export const echo = new Echo({
     broadcaster: "pusher",
     key: PUSHER_APP_KEY,
     cluster: PUSHER_APP_CLUSTER,
-    wsHost: "127.0.0.1",
-    wsPort: 6001,
-    disableStats: true, // optional, to disable stats collection
-    encrypted: false, // optional, set to true if using HTTPS
-    forceTLS: false, // optional, set to true if using HTTPS
-    enabledTransports: ["ws"], // optional, use 'ws' only for WebSocket
+    forceTLS: true,
+    encrypted: true,
 });
